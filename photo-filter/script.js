3d46e0ac15f5b923d5779bf5f204bf1960672902
katiandrews <andryushina.eka@gmail.com>
2021-04-21 11:27:23 +0200

    feat: add reset and loading picture

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -5,6 +5,8 @@ const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.
                 '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 let i = 0;
 const buttonNext = document.querySelector('.btn-next');
+const buttonReset = document.querySelector('.btn-reset');
+const fileInput = document.querySelector('input[type="file"]');
 
 // change output value and add filter
 filters.addEventListener('input', (event) => {
@@ -50,9 +52,31 @@ function getImage() {
 // change picture when 'next picture' is clicked
 buttonNext.addEventListener('click', getImage);
 
+// reset filters
+buttonReset.addEventListener('click', function() {
+  for (let i = 0; i < filters.children.length; i++) {
+    let currentInput = filters.children[i].children[0];
+    let result = currentInput.nextElementSibling;
+    if (currentInput.matches('input[name="saturate"]')) {
+      result.value = 100;
+      currentInput.value = 100;
+      let suffix = currentInput.dataset.sizing || '';
+      img.style.setProperty(`--${currentInput.name}`, 100 + suffix);
+    } else {
+      result.value = 0;
+      currentInput.value = 0;
+      let suffix = currentInput.dataset.sizing || '';
+      img.style.setProperty(`--${currentInput.name}`, 0 + suffix);
+    }
+  }
+});
 
-
-
-
-
-getImage();
+// load picture from user's device 
+fileInput.addEventListener('change', function() {
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+  reader.onload = function() {
+    img.src = reader.result;
+  }
+  reader.readAsDataURL(file);
+})
